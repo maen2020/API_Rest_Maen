0c21932fa8144b7361cae12f8eb012bbc959bceb
maen2020 <maen210212@gmail.com>
2025-08-10 19:20:06 -0600

    Start of the Api Rest project

---
+++ src/main/java/com/maen/api_rest/ProjectApiRestApplication.java
@@ -0,0 +1,12 @@
+package com.maen.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiRestApplication.class, args);
+	}
+}
